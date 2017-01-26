834a5821665fe3fd49ade451b0fd7576210f9009
rcasey_92 <Ryan.c4@me.com>
2017-01-26 15:51:41 -0400

    Removed backgrounds from crate images
    Mad fly crates look dope now

--- src/Crates.js
+++ src/Crates.js
@@ -5,9 +5,9 @@ class Crates extends Component {
   render() {
     return (
         <div className="Crates">
-          <img src={require('./img/cc1.jpg')} role="presentation" />
-          <img src={require('./img/cc2.jpg')} role="presentation" />
-          <img src={require('./img/cc3.jpg')} role="presentation" />
+          <img src={require('./img/cc1-v2.jpg')} role="presentation" />
+          <img src={require('./img/cc2-v2.jpg')} role="presentation" />
+          <img src={require('./img/cc3-v2.jpg')} role="presentation" />
         </div>
     );
   }
