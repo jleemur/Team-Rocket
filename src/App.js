545180f9a24feaa9b5d131074dcede781b469c4e
Jonathan Murray <jon.lee.murray@gmail.com>
2017-01-25 22:59:19 -0400

    cleaned up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import Search from './Search.js';
+//import Search from './Search.js';
 import TabManager from './TabManager.js';
 import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   constructor(props) {
@@ -21,8 +20,11 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to Team Rocket!</h2>
         </div>
-        <div className="App-body">
+        <div className="Tabs">
           <TabManager selectedTab={this.state.selectedTab}/>
+        </div>
+        <div className="App-body">
+        
         </div>
       </div>
     );
