9c4a434f333e2e93450f5e198409207af271d504
Jonathan Murray <jmurray@localhost.localdomain>
2017-01-27 21:48:31 -0400

    Added crate+content. CASH ME OUTSIDE HOW BAH DAT!

--- src/Crates.js
+++ src/Crates.js
@@ -1,14 +1,83 @@
 import React, { Component } from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
 
 class Crates extends Component {
 
+
   render() {
     return (
-        <div className="Crates">
-          <img src={require('./img/cc1-v2.jpg')} role="presentation" />
-          <img src={require('./img/cc2-v2.jpg')} role="presentation" />
-          <img src={require('./img/cc3-v2.jpg')} role="presentation" />
-        </div>
+        <Grid fluid={true}>
+          <Row>
+            <Col className="col1" sm={6} md={3}>
+              <img src={require('./img/cc1/cc1.png')} role="presentation" />
+              <img src={require('./img/cc1/wheel_photon.png')} role="presentation" />
+              <img src={require('./img/cc1/wheel_looper.png')} role="presentation" />
+              <img src={require('./img/cc1/body_dominus_gt.png')} role="presentation" />
+              <img src={require('./img/cc1/body_takumi_rxt.png')} role="presentation" />
+              <img src={require('./img/cc1/trail_trinity.png')} role="presentation" />
+              <img src={require('./img/cc1/wheel_chakram.png')} role="presentation" />
+              <img src={require('./img/cc1/decal_vice.png')} role="presentation" />
+              <img src={require('./img/cc1/decal_snakeskin.png')} role="presentation" />
+              <img src={require('./img/cc1/decal_shibuya.png')} role="presentation" />
+              <img src={require('./img/cc1/decal_pollocaliente.png')} role="presentation" />
+              <img src={require('./img/cc1/decal_dotmatrix.png')} role="presentation" />
+              <img src={require('./img/cc1/decal_combo.png')} role="presentation" />
+              <img src={require('./img/cc1/decal_arcana.png')} role="presentation" />
+              <img src={require('./img/cc1/decal_anubis.png')} role="presentation" />
+            </Col>
+            <Col className="col2" sm={6} md={3}>
+              <img src={require('./img/cc2/cc2.png')} role="presentation" />
+              <img src={require('./img/cc2/wheel_lightning.png')} role="presentation" />
+              <img src={require('./img/cc2/wheel_lobo.png')} role="presentation" />
+              <img src={require('./img/cc2/body_roadhog_xl.png')} role="presentation" />
+              <img src={require('./img/cc2/body_xdevil_mk2.png')} role="presentation" />
+              <img src={require('./img/cc2/trail_pixelfire.png')} role="presentation" />
+              <img src={require('./img/cc2/trail_polygonal.png')} role="presentation" />
+              <img src={require('./img/cc2/decal_whizzle.png')} role="presentation" />
+              <img src={require('./img/cc2/decal_warlock.png')} role="presentation" />
+              <img src={require('./img/cc2/decal_snakeskin.png')} role="presentation" />
+              <img src={require('./img/cc2/decal_ninelives.png')} role="presentation" />
+              <img src={require('./img/cc2/decal_narwhal.png')} role="presentation" />
+              <img src={require('./img/cc2/decal_dragonlord.png')} role="presentation" />
+              <img src={require('./img/cc2/decal_distortion.png')} role="presentation" />
+              <img src={require('./img/cc2/decal_carbonated.png')} role="presentation" />
+            </Col>
+            <Col className="col3" sm={6} md={3}>
+              <img src={require('./img/cc3/cc3.png')} role="presentation" />
+              <img src={require('./img/cc3/wheel_pulsus.png')} role="presentation" />
+              <img src={require('./img/cc3/wheel_discotheque.png')} role="presentation" />
+              <img src={require('./img/cc3/body_breakout_types.png')} role="presentation" />
+              <img src={require('./img/cc3/trail_darkmatter.png')} role="presentation" />
+              <img src={require('./img/cc3/trail_hypernova.png')} role="presentation" />
+              <img src={require('./img/cc3/wheel_troika.png')} role="presentation" />
+              <img src={require('./img/cc3/decal_turbo.png')} role="presentation" />
+              <img src={require('./img/cc3/decal_snakeskin.png')} role="presentation" />
+              <img src={require('./img/cc3/decal_shisa.png')} role="presentation" />
+              <img src={require('./img/cc3/decal_oni.png')} role="presentation" />
+              <img src={require('./img/cc3/decal_mondo.png')} role="presentation" />
+              <img src={require('./img/cc3/decal_mg-88.png')} role="presentation" />
+              <img src={require('./img/cc3/decal_falchion.png')} role="presentation" />
+              <img src={require('./img/cc3/decal_distortion.png')} role="presentation" />
+            </Col>
+            <Col className="col4" sm={6} md={3}>
+              <img src={require('./img/cc4/cc4.png')} role="presentation" />
+              <img src={require('./img/cc4/wheel_zomba.png')} role="presentation" />
+              <img src={require('./img/cc4/wheel_ara51.png')} role="presentation" />
+              <img src={require('./img/cc4/body_octane_zsr.png')} role="presentation" />
+              <img src={require('./img/cc4/wheel_fsl.png')} role="presentation" />
+              <img src={require('./img/cc4/trail_proton.png')} role="presentation" />
+              <img src={require('./img/cc4/wheel_spiralis.png')} role="presentation" />
+              <img src={require('./img/cc4/decal_wildfire.png')} role="presentation" />
+              <img src={require('./img/cc4/decal_snakeskin.png')} role="presentation" />
+              <img src={require('./img/cc4/decal_roadkill.png')} role="presentation" />
+              <img src={require('./img/cc4/decal_meanstreak.png')} role="presentation" />
+              <img src={require('./img/cc4/decal_maximon.png')} role="presentation" />
+              <img src={require('./img/cc4/decal_kilowatt.png')} role="presentation" />
+              <img src={require('./img/cc4/decal_hitech.png')} role="presentation" />
+              <img src={require('./img/cc4/decal_distortion.png')} role="presentation" />
+            </Col>
+          </Row>
+        </Grid>
     );
   }
 }
