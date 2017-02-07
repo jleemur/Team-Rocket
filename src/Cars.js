2fa31b818e24609f82db06168c669ddc8bf95e93
Jonathan Murray <jon.lee.murray@gmail.com>
2017-02-06 22:47:37 -0400

    Loading local json data

--- src/Cars.js
+++ src/Cars.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-var fetch = require('node-fetch');
 
 //xlsx source: https://drive.google.com/file/d/0B0GvVmzA91-FR3BabDJGYjFaWlU/view
 //Convert xlsx to json using https://convertexcel.net/excel-to-json
 var data = require('./data/1.25_Car-Stats.json');
+var hitbox = [], hitbox_rank = [], turn = [], turn_rank = [] // local data variables
 
 class Cars extends Component {
 
-  fetchData(data) {
-    fetch('http://127.0.0.1:3000/src/data/1.25_Car-Stats.json')
-      .then(function(res) {
-        return ( <div>res.text()</div> )
-      }).then(function(body) {
-        console.log(body);
-      });
+  componentWillMount() {
+    // load data arrays before component mounts
+    hitbox = data["1.25 Hit Boxes"]
+    hitbox_rank = data["1.25 HB Ranks"]
+    turn = data["1.25 Turn Speeds"]
+    turn_rank = data["1.25 Turn Ranks"]
   }
 
+
   render() {
+    console.log(hitbox[0])
+    // console.log(hitbox_rank)
+    // console.log(turn)
+    // console.log(turn_rank)
+
     return (
         <div className="Cars">
-          <p>Hello World!</p>
+          <p></p>
         </div>
     );
   }
